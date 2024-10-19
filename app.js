f26e3533d0abf4cc40e5b13a970744acdaab1999
porky3n <s10255872@connect.np.edu.sg>
2024-10-19 17:37:14 +0800

    Set-up Nav Bar and Footer HTML Template

--- app.js
+++ app.js
@@ -1,16 +1,34 @@
-const dbConfig = require("./dbConfig");
-const sql = require('mssql');
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
 
-async function queryTestTable() {
-    try {
-        let pool = await sql.connect(dbConfig);
-        let result = await pool.request().query('SELECT * FROM test');
-        console.log('Query result:', result.recordset);
-    } catch (err) {
-        console.error('Error querying test table:', err);
-    } finally {
-        sql.close();
-    }
-}
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
 
-queryTestTable();
+// Route for the index page
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
+
+// const dbConfig = require("./dbConfig");
+// const sql = require('mssql');
+
+// async function queryTestTable() {
+//     try {
+//         let pool = await sql.connect(dbConfig);
+//         let result = await pool.request().query('SELECT * FROM test');
+//         console.log('Query result:', result.recordset);
+//     } catch (err) {
+//         console.error('Error querying test table:', err);
+//     } finally {
+//         sql.close();
+//     }
+// }
+
+// queryTestTable();
