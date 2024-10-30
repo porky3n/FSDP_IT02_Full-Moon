2cef010bff0ea592a8e35aeaac78d1beebb2e6c2
Ahmed <S10258532@connect.np.edu.sg>
2024-10-30 10:08:16 +0800

    Fixed some linking problems

--- public/js/add-child.js
+++ public/js/add-child.js
@@ -1,17 +1,4 @@
 document.addEventListener("DOMContentLoaded", function () {
-  // Load navbar and footer
-  fetch("navbar.html")
-    .then((response) => response.text())
-    .then((data) => {
-      document.getElementById("navbar-placeholder").innerHTML = data;
-    });
-
-  fetch("footer.html")
-    .then((response) => response.text())
-    .then((data) => {
-      document.getElementById("footer-placeholder").innerHTML = data;
-    });
-
   // Store initial form values
   const form = document.getElementById("childProfileForm");
   let initialFormData = {};
