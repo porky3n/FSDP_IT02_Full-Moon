215a65d81b59c58262cb8da45b7f192ef6b8d1e3
Aung04 <s10243228@connect.np.edu.sg>
2024-11-05 06:57:26 +0800

    home page

---
+++ public/js/homepage.js
@@ -0,0 +1,21 @@
+var TrandingSlider = new Swiper('.tranding-slider', {
+  effect: 'coverflow',
+  grabCursor: true,
+  centeredSlides: true,
+  loop: true,
+  slidesPerView: 'auto',
+  coverflowEffect: {
+    rotate: 0, // No rotation effect
+    stretch: 0, // No stretching
+    depth: 100, // Depth of the effect
+    modifier: 2.5, // Increase the effect strength
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
