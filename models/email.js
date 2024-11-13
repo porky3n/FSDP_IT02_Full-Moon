81066b21bc999c3f2add1c5729455c6fcf782162
Vincent <s10257433@connect.np.edu.sg>
2024-11-13 11:54:31 +0800

    Edited paymentReceipt for email

--- models/email.js
+++ models/email.js
@@ -1,4 +1,6 @@
 const nodemailer = require("nodemailer");
+const fs = require("fs");
+const path = require("path");
 
 const transporter = nodemailer.createTransport({
   service: "gmail",
@@ -16,23 +18,43 @@ const transporter = nodemailer.createTransport({
 const sendPaymentConfirmationEmail = async (userDetails) => {
     const { userEmail, programmeName, startDate, endDate, paymentAmount, paymentMethod } = userDetails;
   
+    console.log(__dirname);
+    let htmlContent = fs.readFileSync(path.join(__dirname, '../public/paymentReceipt.html'), 'utf-8');
+    htmlContent = htmlContent
+    .replace("{{receiptDate}}", new Date().toDateString())
+    .replace("{{receiptNumber}}", Math.floor(Math.random() * 10000000000).toString())
+    .replace("{{programmeName}}", programmeName)
+    .replace("{{programmeAmount}}", paymentAmount)
+    // .replace("{{paymentMethod}}", paymentMethod)
+    .replace("{{subTotal}}", paymentAmount)
+    .replace("{{taxFee}}", (paymentAmount * 0.09).toFixed(2))
+    .replace("{{total}}", (paymentAmount * 1.09).toFixed(2))
+    .replace("{{startDate}}", startDate)
+    .replace("{{endDate}}", endDate)
+    
     const mailOptions = {
       from: "",
       to: userEmail,
       subject: "Payment Confirmation and Slot Booking Successful",
-      html: `
-        <div style="font-family: Arial, sans-serif; color: #333;">
-          <h2 style="color: #4CAF50;">Thank you for your payment!</h2>
-          <p>Your slot for <strong>${programmeName}</strong> has been successfully booked.</p>
-          <div style="border: 1px solid #ddd; padding: 10px; margin-top: 15px;">
-            <p><strong>Payment Details:</strong></p>
-            <p>Amount Paid: <span style="color: #4CAF50;">$${paymentAmount}</span></p>
-            <p>Payment Method: ${paymentMethod}</p>
-            <p>Programme Date: ${startDate} - ${endDate}</p>
-          </div>
-          <p style="margin-top: 20px;">We look forward to seeing you there!</p>
-        </div>
-      `
+      // html: `
+      //   <div style="font-family: Arial, sans-serif; color: #333;">
+      //     <h2 style="color: #4CAF50;">Thank you for your payment!</h2>
+      //     <p>Your slot for <strong>${programmeName}</strong> has been successfully booked.</p>
+      //     <div style="border: 1px solid #ddd; padding: 10px; margin-top: 15px;">
+      //       <p><strong>Payment Details:</strong></p>
+      //       <p>Amount Paid: <span style="color: #4CAF50;">$${paymentAmount}</span></p>
+      //       <p>Payment Method: ${paymentMethod}</p>
+      //       <p>Programme Date: ${startDate} - ${endDate}</p>
+      //     </div>
+      //     <p style="margin-top: 20px;">We look forward to seeing you there!</p>
+      //   </div>
+      // `
+      html: htmlContent,
+      attachments: [{
+          filename: 'logo.png',
+          path: path.join(__dirname, '../public/images/mindsphere-logo.png'),
+          cid: 'mindsphere-logo' //same cid value as in the html img src
+      }]
     };
   
     try {
